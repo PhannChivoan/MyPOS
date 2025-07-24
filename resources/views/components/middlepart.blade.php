f6a2c50e7effc0d87aca0c36b849dd1812a98fa9
PhannChivoan <phannchivoan.biu@gmail.com>
2025-07-24 12:28:07 +0700

    added overall responsive

--- resources/views/components/middlepart.blade.php
+++ resources/views/components/middlepart.blade.php
@@ -8,19 +8,17 @@
   </div>
 </div>
 
-  <div class="row justify-content-center gx-3 " id="menu">
-    @foreach($pro as $pros)
-      <div class="col-6 col-sm-4 col-md-3 d-flex mt-2 product-item" data-category="{{ $pros->cate_id ?? '' }}">
-        <div class="card card-style w-100 h-100"onclick="addToCart('{{ $pros->id }}', '{{ $pros->pro_name }}', {{ $pros->pro_price }})">
-          <!-- <img src="images/{{ $pros->pro_pic }}" class="card-img-top" alt="{{ $pros->pro_name }}"  style="height:10rem; object-fit:cover" /> -->
-          <img src="{{ $pros->pro_pic }}"  class="card-img-top" alt="{{ $pros->pro_name }}"  style="height:10rem; object-fit:cover"  alt="Test Image" />
-
-          <div class="card-body text-center">
-            <h5 class="card-title">{{ $pros->pro_name }}</h5>
-            <p class="card-text">{{ $pros->pro_price }}$</p>
-          </div>
+<div class="row justify-content-center gx-3" id="menu">
+  @foreach($pro as $pros)
+    <div class="col-6 col-sm-6 col-md-6 col-lg-3 d-flex mt-2 product-item" data-category="{{ $pros->cate_id ?? '' }}">
+      <div class="card card-style w-100 h-100" onclick="addToCart('{{ $pros->id }}', '{{ $pros->pro_name }}', {{ $pros->pro_price }})">
+        <img src="{{ $pros->pro_pic }}" class="card-img-top" alt="{{ $pros->pro_name }}" style="height:10rem; object-fit:cover" />
+        <div class="card-body text-center">
+          <h5 class="card-title">{{ $pros->pro_name }}</h5>
+          <p class="card-text">{{ $pros->pro_price }}$</p>
         </div>
       </div>
-    @endforeach
-  </div>
+    </div>
+  @endforeach
+</div>
 </div>
